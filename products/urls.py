3459c56e6a96402f45bb150ec235affb2f3a77dc
mgreenberry <michaelgreenberry@gmail.com>
2022-02-06 12:46:17 +0000

    corrected views.py and urls.py as mixed up

--- products/urls.py
+++ products/urls.py
@@ -1,15 +1,6 @@
-from django.shortcuts import render
-from .models import Product
+from django.urls import path
+from . import views
 
-# Create your views here.
-
-def all_products(request):
-    """ A view to show all products, including sorting and search queries """
-
-    products = Product.objects.all()
-
-    context = {
-        'products': products,
-    }
-
-    return render(request, 'products/products.html', context)
+urlpatterns = [
+    path('', views.all_products, name='products')
+]
