4d6c6de4bb75f655ce4f2d1ee04c893cb3056248
mgreenberry <michaelgreenberry@gmail.com>
2022-02-06 11:11:48 +0000

    customised admin and added extra fields

--- confectionary/admin.py
+++ confectionary/admin.py
@@ -1,6 +1,27 @@
 from django.contrib import admin
 from .models import Confectionary, Category
 
-# Register your models here.
-admin.site.register(Confectionary)
-admin.site.register(Category)
+
+class ConfectionaryAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+        'favourite',
+    )
+
+    ordering = ('name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Confectionary, ConfectionaryAdmin)
+admin.site.register(Category, CategoryAdmin)
--- confectionary/models.py
+++ confectionary/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 # Create your models here.
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -14,6 +18,10 @@ class Category(models.Model):
 
 
 class Confectionary(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Confectionary'
+
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
